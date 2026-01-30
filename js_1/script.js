1950dd6ba3af353d1772d289e6da4facc033e973
Feyli <mathiscom64@gmail.com>
2026-01-30 15:17:47 +0100

    fix: update message handling and improve accessibility for dynamic content display

--- js_1/script.js
+++ js_1/script.js
@@ -1,36 +1,52 @@
+const INTERVAL_TIME = 10000;
+
 const A_randomNumbers = Array.from({ length: 20 }, () => Math.floor(Math.random() * 51) - 10);
+
+const buildMessageNode = (content) => {
+    if (typeof content !== 'string') throw new Error('Only a string should be passed to this function');
+
+    const E_result = document.createElement('p');
+    E_result.classList.add('message');
+    E_result.textContent = content;
+    E_result.role = 'alert';
+    E_result.ariaLive = 'polite';
+    return E_result;
+}
 
 function main() {
     let I_counter = -1;
 
     const E_jsValue = document.querySelector('#jsValue');
-    const E_message = document.querySelector('#message');
 
     const updateInterval = setInterval(() => {
         I_counter++;
         const S_value = A_randomNumbers[I_counter].toString();
-        updateDOM(E_jsValue, E_message, S_value);
+        updateDOM(E_jsValue, S_value);
         if (I_counter >= A_randomNumbers.length - 1) clearInterval(updateInterval);
-    }, 1000);
+    }, INTERVAL_TIME);
 
-    const updateDOM = (O_node, O_messageNode, S_value) => {
+    const updateDOM = (O_node, S_value) => {
+        // Remove all existing message nodes (in case there are any)
+        const A_messageNodes = document.querySelectorAll('.message');
+        A_messageNodes.forEach((e) => e.remove());
+
         O_node.textContent = S_value;
         const I_numericValue = Number.parseInt(S_value);
 
+        // Clear classes from the value node, for a fresh start
         O_node.classList.remove('border-red', 'border-blue', 'border-green', 'border-orange');
 
-        O_messageNode.textContent = '';
-
+        // Assert all conditions and update the DOM accordingly
         if (I_numericValue >= 31 && I_numericValue <= 40) {
-            O_messageNode.textContent = 'Caliente ! Vamos a la playa, ho hoho hoho !!';
+            E_jsValue.parentElement.before(buildMessageNode('Caliente ! Vamos a la playa, ho hoho hoho !!'));
             O_node.classList.add('border-red');
         } else if (I_numericValue >= 1 && I_numericValue <= 20) O_node.classList.add('border-green');
         else if (I_numericValue >= 21 && I_numericValue <= 30) O_node.classList.add('border-orange');
         else if (I_numericValue >= -10 && I_numericValue <= 0) {
-            O_messageNode.textContent = 'Brrrrrrr, un peu froid ce matin, mets ta cagoule !';
+            E_jsValue.parentElement.before(buildMessageNode('Brrrrrrr, un peu froid ce matin, mets ta cagoule !'));
             O_node.classList.add('border-blue');
         }
     };
 }
 
-window.onload = main;
+main();
